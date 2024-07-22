eb55ca8c11f4a9a643ac47ae94fa58481814e3d7
Liliana <lilianakoric@gmail.com>
2024-07-21 21:23:37 -0700

    Add allure report

--- tests/main.py
+++ tests/main.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pytest
@@ -14,6 +16,7 @@ def test_text_and_email(browser):
     browser.get('https://go.teamsideline.com/')
     link = browser.find_element(By.XPATH, '//a[text()="Text, Email, Chat Communications Notifications & More"]')
     link.click()
+    time.sleep(2)
     header = browser.find_element(By.XPATH, '//h1[@class="page-header"]/span[text()="Text and Email Communications"]')
     assert header.is_displayed(), "Header 'Text and Email Communications' is not visible"
 
